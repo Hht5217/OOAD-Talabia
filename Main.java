b8f9bef3f9d0b3959087045e3da00a404653e4e4
Hht5217 <1211105217@mmu.edu.my>
2023-12-17 03:30:26 +0800

    compile and run Main.java to check the board

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
-public class Main{
-    public static void main(string[] args){
-        Game game = new Game();
-        game.gameLoop();
+public class Main {
+    public static void main(String[] args) {
+        new View();
     }
 }
--- View.java
+++ View.java
@@ -1,3 +1,34 @@
-public class View {
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
 
+public class View {
+    private JButton[] chessButtons = new JButton[42];
+    private int chessButtonsIndex = 0;
+
+    public View() {
+        JFrame talabiaFrame = new JFrame("Talabia");
+        talabiaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        talabiaFrame.setSize(700, 600);
+        talabiaFrame.setLocationRelativeTo(null);
+
+        JPanel boardPanel = new JPanel();
+        boardPanel.setLayout(new GridLayout(6, 7));
+        for (int r = 1; r <= 6; r++) {
+            for (int c = 1; c <= 7; c++) {
+                JButton chessButton = new JButton("r" + r + "c" + c);
+                chessButton.setText(null);
+                if (chessButtonsIndex % 2 == 0) {
+                    chessButton.setBackground(Color.WHITE);
+                } else {
+                    chessButton.setBackground(Color.BLACK);
+                }
+                chessButtons[chessButtonsIndex] = chessButton;
+                chessButtonsIndex++;
+                boardPanel.add(chessButton);
+            }
+        }
+        talabiaFrame.add(boardPanel);
+        talabiaFrame.setVisible(true);
+    }
 }
