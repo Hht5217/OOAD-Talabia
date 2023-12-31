8dad13d560d17f0c762d15c0c05b7868f88ce7a8
Hht5217 <1211105217@mmu.edu.my>
2023-12-31 19:42:14 +0800

    changes

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Model class
 public class Board {
     private Piece[][] pieces;
 
@@ -7,41 +8,75 @@ public class Board {
         this.pieces = new Piece[6][7];
     }
 
-    public boolean isEmptySpace(int xPos, int yPos) {
-        if (pieces[xPos][yPos] == null) {
+    // Check if specific location is empty
+    public boolean isEmptySpace(int yPos, int xPos) {
+        if (pieces[yPos][xPos] == null) {
             return true;
         }
         return false;
     }
 
+    // Add chess piece to board
     public void addPiece(Piece pieceToAdd) {
-        int addX = pieceToAdd.getXPos();
         int addY = pieceToAdd.getYPos();
-        pieces[addX][addY] = pieceToAdd;
+        int addX = pieceToAdd.getXPos();
+        pieces[addY][addX] = pieceToAdd;
+    }
+
+    // Move piece to new position
+    public void movePiece(Piece p, int y, int x) {
+        // Get old position of piece
+        int oldYPos = p.getYPos();
+        int oldXPos = p.getXPos();
+        pieces[y][x] = p; // Place the piece at the new position
+        p.setPos(y, x); // Update the value of y and x of piece
+        pieces[oldYPos][oldXPos] = null; // Remove piece from old position
     }
 
     // Check if the piece is within the board
-    public boolean inBoard(int xPos, int yPos){
-		return xPos >= 0 && xPos < getX() && yPos >= 0 && yPos < getY();
-	}
+    public boolean inBoard(int yPos, int xPos) {
+        return xPos >= 0 && xPos < getX() && yPos >= 0 && yPos < getY();
+    }
 
     // Check status of specific location on board
-    public Piece pieceLocation(int xPos, int yPos){
-		if (inBoard(xPos, yPos)){
-			return pieces[xPos][yPos];
-		}
-		return null;
-	}
+    public Piece pieceLocation(int yPos, int xPos) {
+        if (inBoard(yPos, xPos)) {
+            return getPiece(yPos, xPos);
+        }
+        return null;
+    }
 
+    // Get length X (row) of board
     public int getX() {
         return pieces[0].length;
     }
 
+    // Get length Y (column) of board
     public int getY() {
         return pieces.length;
     }
 
-    public Piece getPiece(int pieceX, int pieceY) {
-        return pieces[pieceX][pieceY];
+    // Return piece at specific location
+    public Piece getPiece(int yPos, int xPos) {
+        return pieces[yPos][xPos];
+    }
+
+    // Return piece array
+    public Piece[][] getBoard() {
+        return pieces;
+    }
+
+    // Test purpose
+    public void printBoard() {
+        for (int i = 0; i < pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
+                if (isEmptySpace(i, j)) {
+                    System.out.print("null |");
+                } else {
+                    System.out.print(pieces[i][j].toString() + "|");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Model m = new Model();
-        new View(m);
+        Game g = new Game(); // One of model
+        // Model m = new Model();
+        View v = new View(g); // View display the model in GUI
+        Controller controller = new Controller(g, v); // Controller updates model after getting actions from view
+        controller.initController();
     }
 }
