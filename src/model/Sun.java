236c1be475f2267d708bec1672de50d7cf81305e
Hht5217 <1211105217@mmu.edu.my>
2024-01-03 00:28:19 +0800

    working sun pieces

--- src/model/Sun.java
+++ src/model/Sun.java
@@ -12,6 +12,22 @@ public class Sun extends Piece {
     // public void availableMoves() {
     public List<Move> getAvailableMoves() {
         // move logic
+        movePos.clear();
+        int currentY = getYPos();
+        int currentX = getXPos();
+
+        // Explore all directions in a single loop with checks for out-of-bounds and
+        // occupied spaces
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int potentialY = currentY + i;
+                int potentialX = currentX + j;
+
+                if (pieceBoard.inBoard(potentialY, potentialX) && pieceBoard.isEmptySpace(potentialY, potentialX)) {
+                    movePos.add(new Move(potentialY, potentialX));
+                }
+            }
+        }
         return movePos;
     }
 }
