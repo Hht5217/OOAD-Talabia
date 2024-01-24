c08a1399e860c76dabfb9376b0f87594e19d5d0d
Hht5217 <1211105217@mmu.edu.my>
2024-01-24 18:04:46 +0800

    name changed

---
+++ src/pieces/Hourglass.java
@@ -0,0 +1,50 @@
+package pieces;
+
+import java.util.List;
+import model.*;
+
+public class Hourglass extends Piece {
+    public Hourglass(String id, int yPos, int xPos, PlayerColor color, Board pieceBoard) {
+        super(id, yPos, xPos, color, pieceBoard);
+    }
+
+    /*
+     * Check if piece is allowed to move, add if allowed and do nothing if not an
+     * available move
+     */
+    @Override
+    // public void availableMoves() {
+    public List<Move> getAvailableMoves() {
+        moveList.clear();
+        int currentY = getYPos();
+        int currentX = getXPos();
+
+        /*
+         * Define all possible L-shaped moves for the Hourglass piece in pair of y and x
+         * positions, then add them to the current position of the piece.
+         */
+        int[][] offsets = {
+                { -2, -1 }, { -2, 1 },
+                { -1, -2 }, { -1, 2 },
+                { 1, -2 }, { 1, 2 },
+                { 2, -1 }, { 2, 1 }
+        };
+
+        for (int[] offset : offsets) {
+            int potentialY = currentY + offset[0];
+            int potentialX = currentX + offset[1];
+
+            // Check if the move is within the bounds of the board
+            if (pieceBoard.inBoard(potentialY, potentialX)) {
+                // Check if the square is either empty or contains an opponent's piece
+                if (pieceBoard.isEmptySpace(potentialY, potentialX) ||
+                        !pieceBoard.getPiece(potentialY, potentialX).getColor().equals(this.getColor())) {
+                    moveList.add(new Move(potentialY, potentialX));
+                }
+            }
+        }
+
+        return moveList;
+    }
+
+}
